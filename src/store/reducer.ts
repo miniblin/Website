b0733a6dc022ecca64144f46f2701b8c65802b9d
David Lewis <dil20@sussex.ac.uk>
2018-04-28 23:53:45 +0100

    Set up Redux.

---
+++ src/store/reducer.ts
@@ -0,0 +1,10 @@
+const DEFAULT_STATE = { }
+
+const rootReducer = (state = DEFAULT_STATE, action: any) => {
+    switch (action.type) {
+        
+        default: return state
+    }
+} 
+
+export default rootReducer 
