7b3b7bdab1b1ea843de5bd4033e897fcfbbf5ce9
David Lewis <dil20@sussex.ac.uk>
2018-04-30 20:01:11 +0100

    Fix tsconfig for css modules.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,13 @@
 import * as React from "react";
-
+import styles from './App.scss'
 import Footer from './Footer'
 export interface AppProps { }
 import Hero from './Hero'
 export default function App(props: AppProps) {
     return (
-        <>
+        <div className={styles.container}>
             <Hero />
             <Footer />
-        </>
+        </div>
     )
 }  
--- webpack.config.js
+++ webpack.config.js
@@ -5,101 +5,70 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    entry: "./src/index.tsx",
-    output: {
-        filename: "bundle.js",
-        path: __dirname + "/dist"
-    },
-    // Enable sourcemaps for debugging webpack's output.
-    devtool: "source-map",
-    resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions. 
-        extensions: [".ts", ".tsx", ".js", ".json"]
-    }, module: {
-        rules: [
-            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.    
-            {
-                test: /\.tsx?$/,
-                loader: "awesome-typescript-loader"
-            },
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.     
-            {
-                enforce: "pre",
-                test: /\.js$/, loader: "source-map-loader"
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: 'style-loader'
-                    },
-                    {
-                        loader: 'css-loader',
-                        query: {
-                            sourceMap: true
-                        }
-                    }
-                ],
-            },
-            {
-                test: /\.scss$/,
-                include: /fonts/,
-                use: [
-                    {
-                        loader: 'style-loader'
-                    },
-                    {
-                        loader: 'css-loader',
-                        query: {
-                            sourceMap: true
-                        }
-                    },
-                    {
-                        loader: 'sass-loader'
-                    }
-                ]
-            },
-            {
-                test: /\.scss$/,
-                exclude: /fonts/,
-                use: [
-                    {
-                        loader: 'style-loader'
-                    },
-                    {
-                        loader: 'css-loader',
-                        query: {
-                            modules: true,
-                            sourceMap: true,
-                            localIdentName: '[name]__[local]___[hash:base64:5]'
-                        }
-                    },
-                    {
-                        loader: 'sass-loader'                        
-                    }
-                ]
-            }           
-        ]
-    },
-    plugins: [
-        new CleanWebpackPlugin(['dist']),
-        new HtmlWebpackPlugin({
-            title: 'website',
-            template: path.resolve('index.html')
-        }),       
-        new webpack.NamedModulesPlugin(),
-        new webpack.HotModuleReplacementPlugin()
-    ],
-    devServer: {
-        hot: true,
-        inline: true,
-        historyApiFallback: true
-    },
-    // When importing a module whose path matches one of the following, just assume a corresponding global 
-    // variable exists and use that instead. This is important because it allows us to avoid bundling all 
-    // of our dependencies, which allows browsers to cache those libraries between builds 
-    externals: {
-        "react": "React",
-        "react-dom": "ReactDOM"
-    },
-}; 
+   entry: ['babel-polyfill', path.resolve('src/index.tsx')],
+   output: {
+       filename: 'index.js',
+       path: path.resolve('build')
+   },
+   devtool: 'inline-source-map',
+   resolve: {
+       modules: [path.resolve('src'), 'node_modules'],
+       extensions: ['.ts', '.tsx', '.js']
+   },
+   module: {
+       rules: [
+           {
+               test: /\.tsx?$/,
+               use: [
+                   {
+                       loader: 'awesome-typescript-loader'
+                   }
+               ]
+           },            
+           {
+               test: /\.css$/,
+               use: [
+                   {
+                       loader: 'style-loader'
+                   },
+                   {
+                       loader: 'css-loader',
+                       query: {
+                           sourceMap: true
+                       }
+                   }
+               ],
+           },
+           {
+               test: /\.scss$/,
+               exclude: /fonts/,
+               use: [
+                   {
+                       loader: 'style-loader'
+                   },
+                   {
+                       loader: 'css-loader',
+                       query: {
+                           modules: true,
+                           sourceMap: true,
+                           localIdentName: '[name]__[local]___[hash:base64:5]'
+                       }
+                   },
+                   {
+                       loader: 'sass-loader',                        
+                   }
+               ]
+           }          
+       ]
+   },
+   plugins: [
+       new HtmlWebpackPlugin({
+           title: 'Digiterre React Project Template',
+           template: path.resolve('./src/index.html')
+       })            
+   ],
+   devServer: {      
+       inline: true,
+       historyApiFallback: true
+   }
+};
